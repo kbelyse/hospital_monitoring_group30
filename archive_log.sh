90bf55e13a6c6876d45be6c6716c5c7c2e680ec2
dusengepeggy <dickinepeggy100@gmail.com>
2025-02-05 13:17:53 +0200

    archiving the files

--- archive_log.sh
+++ archive_log.sh
@@ -6,3 +6,9 @@ if [ ! -f "log_file" ]; then
 	echo "error '$log_file' not found!"
 	exit1
 fi
+
+
+timestamp=$(date "+%Y%m%d_%H%M%S")
+archive_file="${log_file}_${timestamp}"
+mv "$log_file" "$archive_file"
+echo "Log file archived as: $archive_file"
