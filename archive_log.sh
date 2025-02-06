075fc34149e2f9bd407ca3a893ef4bdbd01727a5
bradshema <rugabrad@gmail.com>
2025-02-06 12:58:09 +0200

    Fixed (file not found) error.

--- archive_log.sh
+++ archive_log.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-
-log_file=hear_rate_log.txt
-
-if [ ! -f "log_file" ]; then
+log_file=heart_rate_log.txt
+if [ ! -f "$log_file" ]; then
 	echo "error '$log_file' not found!"
-	exit1
+	exit 1
 fi
-
-
 timestamp=$(date "+%Y%m%d_%H%M%S")
 
 archive_file="${log_file}_${timestamp}"
